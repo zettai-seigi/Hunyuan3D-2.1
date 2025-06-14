d575f308d0d8bf31de3958de2923a5a962165f5d
Huiwenshi <shihuiwen97@gmail.com>
2025-06-14 14:53:27 +0800

    fix

--- demo.py
+++ demo.py
@@ -18,17 +18,17 @@ except Exception as e:
     print(f"Warning: Failed to apply torchvision fix: {e}")
 
 # shape
-#model_path = 'tencent/Hunyuan3D-2.1'
-#pipeline_shapegen = Hunyuan3DDiTFlowMatchingPipeline.from_pretrained(model_path)
-##
-#image_path = 'assets/demo.png'
-#image = Image.open(image_path).convert("RGBA")
-#if image.mode == 'RGB':
-#    rembg = BackgroundRemover()
-#    image = rembg(image)
-#
-#mesh = pipeline_shapegen(image=image)[0]
-#mesh.export('demo.glb')
+model_path = 'tencent/Hunyuan3D-2.1'
+pipeline_shapegen = Hunyuan3DDiTFlowMatchingPipeline.from_pretrained(model_path)
+
+image_path = 'assets/demo.png'
+image = Image.open(image_path).convert("RGBA")
+if image.mode == 'RGB':
+    rembg = BackgroundRemover()
+    image = rembg(image)
+
+mesh = pipeline_shapegen(image=image)[0]
+mesh.export('demo.glb')
 
 # paint
 max_num_view = 6  # can be 6 to 9
